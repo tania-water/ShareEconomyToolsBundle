586e3a0af7d8ee0dae7b21f25a791c930d665e42
Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
2016-10-20 12:14:05 +0200

    Enhancement: Add new trackable treat

---
+++ Entity/TrackableTrait.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Ibtikar\ShareEconomyToolsBundle\Entity;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+
+trait TrackableTrait
+{
+
+    /**
+     * @var \DateTime $created
+     *
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var \DateTime $updated
+     *
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(type="datetime")
+     */
+    private $updated;
+
+    /**
+     * @var int
+     *
+     * @ORM\Version
+     * @ORM\Column(type="integer")
+     */
+    private $version = 1;
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return this
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param \DateTime $updated
+     *
+     * @return this
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return \DateTime
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * Set version
+     *
+     * @param integer $version
+     *
+     * @return this
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+
+        return $this;
+    }
+
+    /**
+     * Get version
+     *
+     * @return integer
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+}
