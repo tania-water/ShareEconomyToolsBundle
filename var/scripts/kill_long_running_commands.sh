45f2f3b9070acf5459277a926547603255c4ff38
Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
2016-12-28 11:20:27 +0200

    Enhancement: Correct the path of the long running commands process files and add extra information printing

--- var/scripts/kill_long_running_commands.sh
+++ var/scripts/kill_long_running_commands.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
-FILES="$BASEDIR/../run/*"
+FILES="$BASEDIR/../../../../../var/run/*"
 for f in $FILES
 do
     if [ -f "$f" ]; then
         read PID < "$f"
+        echo "Killing the process number "$PID
         kill -9 $PID > /dev/null 2> /dev/null &
         rm "$f"
     fi
